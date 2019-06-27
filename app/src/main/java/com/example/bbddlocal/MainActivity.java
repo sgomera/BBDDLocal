88963a0a5fffa36c273c16c828f6b2331f8e7cc1
sgomera <silviagomera@gmail.com>
2019-06-27 13:02:14 +0200

    InProcess subscribe in MainActivity

--- app/src/main/java/com/example/bbddlocal/MainActivity.java
+++ app/src/main/java/com/example/bbddlocal/MainActivity.java
@@ -2,12 +2,27 @@ package com.example.bbddlocal;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.arch.lifecycle.ViewModelProviders;
+
+
 
 public class MainActivity extends AppCompatActivity {
+    private AnimalsViewModel mViewModel;
+    private TextView mAnimalsTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Get a reference to the ViewModel for this screen.
+        mViewModel = ViewModelProviders.of(this).get(AnimalsViewModel.class);
+
+    }
+
+    public void onRefreshBtClicked(View view) {
+        mViewModel.createDb();
     }
 }
