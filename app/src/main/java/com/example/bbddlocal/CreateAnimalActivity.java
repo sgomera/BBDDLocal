b5683d33a71b4d77f46a4f42eb3e7d1726ae7284
sgomera <silviagomera@gmail.com>
2019-07-09 13:31:40 +0200

    Photo from camera or file in process DatePicker in process Search animal not working

--- app/src/main/java/com/example/bbddlocal/CreateAnimalActivity.java
+++ app/src/main/java/com/example/bbddlocal/CreateAnimalActivity.java
@@ -1,7 +1,13 @@
 package com.example.bbddlocal;
 
+import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.arch.lifecycle.ViewModelProviders;
 import android.content.Intent;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -17,7 +23,9 @@ import com.example.bbddlocal.bbdd.Animal;
 import com.example.bbddlocal.bbdd.AnimalsViewModel;
 import com.example.bbddlocal.utils.DatePickerFragment;
 
+import java.io.File;
 import java.io.Serializable;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
@@ -26,11 +34,16 @@ public class CreateAnimalActivity extends AppCompatActivity {
     //variables for EditText fields
     private EditText animalId;
     private ImageView imgAnimal;
+    private Button takePictureButton;
+    private Button selectFileButton;
     private EditText animalName;
     private EditText animalAge;
     private EditText regDate;
     private EditText animalType;
     private CheckBox isChipped;
+
+    private Uri file;
+
 
 
    // public static final String EXTRA_REPLY = "com.example.android.animallistsql.REPLY";
@@ -52,6 +65,10 @@ public class CreateAnimalActivity extends AppCompatActivity {
         regDate = findViewById(R.id.et_reg_date);
         animalType = findViewById(R.id.et_animal_type);
         isChipped = findViewById(R.id.checkBox);
+
+        takePictureButton = findViewById(R.id.button_image);
+        selectFileButton = findViewById(R.id.button_file);
+        imgAnimal = findViewById(R.id.img_animal);
 
 
         // Get a reference to the ViewModel for this screen (activity).
@@ -119,5 +136,76 @@ public class CreateAnimalActivity extends AppCompatActivity {
 
         return animal;
     }
+
+    //mètode per fer la foto
+    public void takePicture(View view) {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        file = Uri.fromFile(getOutputMediaFile());
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, file);
+
+        startActivityForResult(intent, 100);
+    }
+
+    @SuppressLint("LongLogTag")
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 100) {
+            if (resultCode == RESULT_OK) {
+                imgAnimal.setImageURI(file);
+            }
+        }
+
+        if (requestCode == 200 && resultCode == Activity.RESULT_OK) {
+            // The document selected by the user won't be returned in the intent.
+            // Instead, a URI to that document will be contained in the return intent
+            // provided to this method as a parameter.
+            // Pull that URI using data.getData().
+            Uri uri = null;
+            if (data != null) {
+                uri = data.getData();
+                imgAnimal.setImageURI(uri);
+                //        Log.i(TAG, "Uri: " + uri.toString());
+                //       showImage(uri);
+            }
+        }
+    }
+
+
+
+
+    //mètode per guardar la foto en un fitxer
+    private static File getOutputMediaFile(){
+        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES), "CameraDemo");
+
+        if (!mediaStorageDir.exists()){
+            if (!mediaStorageDir.mkdirs()){
+                return null;
+            }
+        }
+
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        return new File(mediaStorageDir.getPath() + File.separator +
+                "IMG_"+ timeStamp + ".jpg");
+    }
+
+    //mètodes per sel·leccionar un fitxer: Fires an intent to spin up the "file chooser" UI and select an image
+    public void performFileSearch(View view) {
+
+        // ACTION_OPEN_DOCUMENT is the intent to choose a file via the system's file
+        // browser.
+        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+
+        // Filter to only show results that can be "opened", such as a
+        // file (as opposed to a list of contacts or timezones)
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+
+        // Filter to show only images, using the image MIME data type.
+        intent.setType("image/*");
+
+        startActivityForResult(intent, 200);
+    }
+
 
 }
--- app/src/main/java/com/example/bbddlocal/MainActivity.java
+++ app/src/main/java/com/example/bbddlocal/MainActivity.java
@@ -1,13 +1,26 @@
 package com.example.bbddlocal;
 
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.arch.lifecycle.Observer;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Html;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.arch.lifecycle.ViewModelProviders;
 import android.widget.Toast;
@@ -16,7 +29,9 @@ import com.example.bbddlocal.bbdd.Animal;
 import com.example.bbddlocal.bbdd.AppDatabase;
 import com.example.bbddlocal.bbdd.AnimalsViewModel;
 
+import java.io.File;
 import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 
@@ -27,6 +42,12 @@ public class MainActivity extends AppCompatActivity {
     private TextView mAnimalsTextView;
     private AnimalsViewModel mViewModel;
 
+    private Button takePictureButton;
+    private Button selectFileButton;
+
+    private boolean permissionGranted;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,11 +56,17 @@ public class MainActivity extends AppCompatActivity {
 
         mAnimalsTextView = (TextView) findViewById(R.id.animals_tv);
 
+        //request permissions
+        if (!permissionGranted) {
+            checkPermissions();
+        }
+
         // Get a reference to the ViewModel for this screen (activity).
         mViewModel = ViewModelProviders.of(this).get(AnimalsViewModel.class);
 
         // Update the UI whenever there's a change in the ViewModel's data.
         subscribeUiAnimals();
+
     }
 
     private void subscribeUiAnimals() {
@@ -99,5 +126,67 @@ public class MainActivity extends AppCompatActivity {
     public void onClickSearch(View view) {
         Intent intent = new Intent(this, SearchAnimalActivity.class);
         startActivity(intent);
+    }
+
+
+
+
+    // Initiate check for permissions.
+    private boolean checkPermissions() {
+
+        int permissionCheckCamera = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.CAMERA);
+        int permissionCheckReadStorage = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_EXTERNAL_STORAGE);
+
+        if ((permissionCheckCamera != PackageManager.PERMISSION_GRANTED) || permissionCheckReadStorage != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.CAMERA, Manifest.permission.READ_EXTERNAL_STORAGE},
+                    0);
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+
+    // Handle permissions result
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String permissions[],
+                                           @NonNull int[] grantResults) {
+        switch (requestCode) {
+
+            case 0:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED
+                    //          && grantResults[1] == PackageManager.PERMISSION_GRANTED
+                ) {
+                    Toast.makeText(this, "Camera permission granted",
+                            Toast.LENGTH_SHORT).show();
+                    takePictureButton.setEnabled(true);
+                    if (grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+                        selectFileButton.setEnabled(true);
+                        Toast.makeText(this, "Read storage permission granted",
+                                Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(this, "Select file button disabled", Toast.LENGTH_SHORT).show();
+                        selectFileButton.setEnabled(false);
+                    }
+                    break;
+                } else {
+                    Toast.makeText(this, "Take picture button disabled", Toast.LENGTH_SHORT).show();
+                    takePictureButton.setEnabled(false);
+                    if (grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+                        selectFileButton.setEnabled(true);
+                        Toast.makeText(this, "Read storage permission granted",
+                                Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(this, "Select file button disabled", Toast.LENGTH_SHORT).show();
+                        selectFileButton.setEnabled(false);
+                    }
+                    break;
+                }
+
+        }
     }
 }
